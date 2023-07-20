4bbdf8f6301f54109142b0e97933b287f2a39028
Gabriel Alves <gabriel52.alves@gmail.com>
2023-07-20 17:53:49 -0300

    Init

---
+++ app-conselhos/src/components/HeaderDefault.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="all">
+        <h1 class="title">Bem vindo ao App-Conselhos</h1>
+    </div>
+</template>
+
+<style>
+
+.all {
+    background-color: #114B5F;
+    height: 120px;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.title {
+    color: #F7F7F7;
+    text-align: center;
+    font-family: 'Raleway', 'Times New Roman', Times, serif;
+    font-weight: 450;
+}
+</style>
