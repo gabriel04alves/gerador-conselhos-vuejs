2c49d2ac3ff54da36c889c0c3bd69e19a04d58a2
Gabriel Alves <gabriel52.alves@gmail.com>
2023-07-22 14:00:55 -0300

    Conteúdo

---
+++ app-conselhos/src/components/ContentMain.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="main">
+        <div class="conselho">
+            <p>Conselho</p>
+        </div>
+    </div>
+</template>
+
+<style>
+.main {
+    width: 100%;
+    height: 100%;
+}
+.conselho {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 10%;
+    width: 75%;
+    background-color: #002f40dc;
+    color: #ececec;
+    font-size: larger;
+    font-family: 'Times New Roman', Times, serif, Arial, Helvetica, sans-serif;
+    font-style: italic;
+    border-radius: 10px;
+    box-shadow: 5px 10px 25px 0px rgba(0,0,0,0.75);
+    -webkit-box-shadow: 5px 10px 25px 0px rgba(0,0,0,0.75);
+    -moz-box-shadow: 5px 10px 25px 0px rgba(0,0,0,0.75);
+}
+</style>
--- app-conselhos/src/views/HomeView.vue
+++ app-conselhos/src/views/HomeView.vue
@@ -1,7 +1,17 @@
 <script setup>
+  import ContentMain from '../components/ContentMain.vue';
 </script>
 
 <template>
   <main>
+    <ContentMain class="main"></ContentMain>
   </main>
 </template>
+
+<style>
+.main {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
