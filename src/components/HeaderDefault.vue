7fca77f0bfed05d0e55c1993ac02417f402d8d59
Gabriel Alves <gabriel52.alves@gmail.com>
2024-05-22 17:53:17 -0300

    feat: refatoração do header

--- src/components/HeaderDefault.vue
+++ src/components/HeaderDefault.vue
@@ -1,14 +1,18 @@
 <template>
-    <div class="all">
-        <h1 class="title">  
-            <svg-icon type="mdi" :path="path" style="zoom: 150%;"></svg-icon> 
-            <p><RouterLink class="router-link" active-class="active" to="/">App-Conselhos</RouterLink></p> 
-        </h1>
-        <h2 class="subtitle">
-            <svg-icon type="mdi" :path="pathb" style="zoom: 120%;"></svg-icon> 
-            <RouterLink class="router-link" active-class="active" to="/about">About</RouterLink>
-        </h2>
-    </div>
+    <header class="header">
+        <div class="title-container">
+            <h1 class="title">  
+                <svg-icon type="mdi" :path="path" style="zoom: 150%;"></svg-icon> 
+                <p><RouterLink class="item-page-link" active-class="active" to="/">Fun Advice App</RouterLink></p> 
+            </h1>
+        </div>
+        <div class="subtitle-container">
+            <h2 class="subtitle">
+                <svg-icon type="mdi" :path="pathb" style="zoom: 120%;"></svg-icon> 
+                <RouterLink class="item-page-link" active-class="active" to="/about">About</RouterLink>
+            </h2>
+        </div>
+    </header>
 </template>
 
 <script>
@@ -34,44 +38,44 @@ export default {
 </script>
 
 <style>
+.header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        flex-direction: row;
+        
+        padding: 1.5rem;
+        margin: 1rem;
+        border-radius: 20px;
+        
+        background-color: #002F40;
+        font-family: 'Raleway', 'Times New Roman', Times, serif;
+    }
 
-.all {
-    background-color: #002F40;
+    .title, .subtitle {
+        display: flex;
+        align-items: center;
+
+        font-weight: 400;
+        font-size: 1rem;
+
+        color: #F7F7F7;
+    }
+
+    .subtitle {
+        gap: 5px;
+    }
     
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    flex-direction: row;
-    font-family: 'Raleway', 'Times New Roman', Times, serif;
-    padding: 20px;
-    border-bottom: 0.25em solid;
-    border-image: linear-gradient(60deg, #8a2be2, #52b0cf) 1 1;
-}
+    .item-page-link {
+        color: #f7f7f7d0;
+        font-weight: 300;
+        font-size: 2vh;
+        text-decoration: none;
+    }
 
-.title, .subtitle {
-    display: flex;
-    align-items: center;
-    font-weight: 400;
-    font-size: 3vh;
-    color: #F7F7F7;
-    display: flex;
-    text-align: center;
-    vertical-align: middle;
-    flex-direction: row;
-}
-.subtitle {
-    gap: 5px;
-}
-.router-link {
-    color: #f7f7f7d0;
-    font-weight: 300;
-    font-size: 2vh;
-    text-decoration: none;
-}
-
-.active {
-    font-weight: 600;
-    text-shadow: 1px #000;
-}
+    .active {
+        font-weight: 600;
+        text-shadow: 1px #000;
+    }
 
 </style>
