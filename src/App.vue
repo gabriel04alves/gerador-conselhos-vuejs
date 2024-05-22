75ff38a9afbe976c66d2c001f968920525e4b8b2
Gabriel Alves <gabriel52.alves@gmail.com>
2024-05-22 18:54:02 -0300

    feat: refatoração da geração de conselhos

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,6 @@ import FooterDefault from './components/FooterDefault.vue';
 import HeaderDefault from './components/HeaderDefault.vue';
 </script>
 <template>
-  <link href="assets/my-style.css" rel="stylesheet">
   <HeaderDefault/>
   <RouterView class="router-view" />
   <FooterDefault/>
--- src/components/ContentMain.vue
+++ src/components/ContentMain.vue
@@ -1,83 +1,74 @@
 <template>
-    <div class="main">
-        <div class="conselho">
-            <p id="resultado"></p>
-        </div>
-        <button class="btn" id="btnGerarConselho">Nova Frase</button>
+  <section class="section-advice">
+    <div class="advice-container">
+      <p v-if="conselho">{{ conselho }}</p>
+      <div v-else>
+        <p>Nothing yet...</p>
+      </div>
     </div>
+    <button class="btn-generator" @click="getPhrase">Get advice</button>
+  </section>
 </template>
 
-<style>
-.main {
-    width: 100%;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-}
-.conselho {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 10%;
-    width: 75%;
-    background-color: #002f40dc;
-    color: #ececec;
-    font-size: larger;
-    font-family: 'Times New Roman', Times, serif, Arial, Helvetica, sans-serif;
-    font-style: italic;
-    border-radius: 10px;
-    box-shadow: 5px 10px 25px 0px rgba(0,0,0,0.75);
-    -webkit-box-shadow: 5px 10px 25px 0px rgba(0,0,0,0.75);
-    -moz-box-shadow: 5px 10px 25px 0px rgba(0,0,0,0.75);
-    margin-bottom: 3%;
-}
-/* Botão */
-.btn {
- width: 10%;
- height: 5%;
- font-size: 1.1em;
- cursor: pointer;
- background-color: #00a6ff;
- color: #fff;
- border: none;
- border-radius: 5px;
- transition: all .4s;
- font-family: 'Raleway', Times, serif, Arial, Helvetica, sans-serif;
-}
-
-.btn:hover {
- border-radius: 5px;
- transform: translateY(-10px);
- box-shadow: 0 7px 0 -2px #39a2dbab,
-  0 15px 0 -4px #bce7ff,
-  0 16px 10px -3px #39a2db;
-}
-
-.btn:active {
- transition: all 0.2s;
- transform: translateY(-5px);
- box-shadow: 0 2px 0 -2px #002f40dc,
-  0 8px 0 -4px #39a2db,
-  0 12px 10px -3px #39a2db;
-}
-</style>
-
 <script>
-import axios from 'axios';
-    function obterConselhoAleatorio() {
-      axios.get('https://api.adviceslip.com/advice')
-        .then(function (response) {
-          const conselho = response.data.slip.advice;
-          console.log(conselho);
-          const divResultado = document.getElementById("resultado");
-          divResultado.innerText = conselho;
-        })
-        .catch(function (error) {
-          console.error(error);
-        });
+  import axios from 'axios';
+
+  export default {
+    data() {
+      return {
+        conselho: ''
+      };
+    },
+    methods: {
+      getPhrase() {
+        axios.get('https://api.adviceslip.com/advice')
+          .then(response => {
+            this.conselho = response.data.slip.advice;
+          })
+          .catch(error => {
+            console.error(error);
+          });
+      }
     }
-    document.addEventListener("DOMContentLoaded", function () {
-        const btnGerarConselho = document.getElementById("btnGerarConselho");
-        btnGerarConselho.addEventListener("click", obterConselhoAleatorio);
-    });
+  }
 </script>
+
+<style>
+  .section-advice {
+    display: flex;
+    flex-flow: column;
+    gap: 2vh;
+
+    padding: 15vh 0;
+  }
+
+  .advice-container {
+    text-align: center;
+    font-size: 1.5rem;
+    padding: 5vw;
+  }
+
+  .advice-container p {
+    transition: 2s;
+  }
+
+  .btn-generator {
+    padding: 1rem;
+
+    font-size: 1rem;
+    color: #002F50;
+    border: none;
+    border-radius: 10px;
+    transition: 0.5s;
+    cursor: pointer;
+  }
+
+  .btn-generator:hover {
+    color: green;
+    transform: translate(-5px, -5px);
+  }
+
+  .btn-generator:active {
+    color: green;
+  }
+</style>
